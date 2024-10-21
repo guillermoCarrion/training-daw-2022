ff1e5f43c6ca3ac0ab178ff643c1b11744da6970
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-10-21 17:20:28 +0200

    unidad 3 ej 4

--- UD3/04_iterators/main.js
+++ UD3/04_iterators/main.js
@@ -30,29 +30,81 @@ var pilots = [
 ];
 
 function iterateSimple() {
+  
+  for (let i = 0; i < pilots.length; i++) {
+    console.log(pilots[i]);
+  }
+  
+  //pilots.forEach(printPilot);
+
+  /*
+  pilots.forEach(pilots){
+    console.log(pilot.id)
+    sum += pilot.years
+  }
+  */
+
+  /*
+  for (let pilot in pilots){
+    console.log(pilots[pilot]);
+  }
+  */
 }
+
 function iterateForEach() {
+  pilots.forEach(function (pilot){
+    console.log(pilot);
+  })
+
+  
 }
+
 function mapIds() {
+  let res = pilots.map(function (value, index, array){
+    return value.id
+  })
+
+  // return pilots.map((value) => value.id)
 }
+
 function rebels() {
+  return  pilots.filter((value) => {
+    return value.faction === 'Rebels'
+  })
 }
+
 function totalFaction(faction) {
+  return pilots
+    .filter(pilot => pilot.faction === faction)
+    .length
 }
+
 function avgYears(faction) {
+  let selectedFaction = pilots
+                          .filter((value) => value.faction === faction)
+  let suma = 0
+
+  selectedFaction.forEach(function(pilot){
+    suma +=pilot.years
+  })
+  return suma / selectedFaction.length
+
+  /*
+    selectedFaction.refuce((previousValue, currentValue) => )
+  */
 }
 
 // use console.log
 iterateSimple()
 iterateForEach()
-try {
-  assert.deepStrictEqual(mapIds(), [2,8,40,66])
-  assert.deepStrictEqual(rebels(), [pilots[0], pilots[3]])
+// try {
+//   assert.deepStrictEqual(mapIds(), [2,8,40,66])
+//   assert.deepStrictEqual(rebels(), [pilots[0], pilots[3]])
 
-  assert.deepStrictEqual(totalFaction('Rebels'), 2)
+//   assert.deepStrictEqual(totalFaction('Rebels'), 2)
 
-  assert.deepStrictEqual(avgYears('Rebels'), 22.5)
-  assert.deepStrictEqual(avgYears('Empire'), 25)
-} catch (error) {
-  console.error(error)
-}
+//   assert.deepStrictEqual(avgYears('Rebels'), 22.5)
+//   assert.deepStrictEqual(avgYears('Empire'), 25)
+// } catch (error) {
+//   console.error(error)
+// }
