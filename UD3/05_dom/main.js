e62a689b0cf42cf7fe30438249d9ba547d30cb8e
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-10-28 17:22:52 +0100

    ej 6 Ud3

---
+++ UD3/05_dom/main.js
@@ -0,0 +1,15 @@
+/*
+
+document.getElementById(id)
+document.getElementsByTagName(name)
+document.getElementsByClassName(name)
+document.querySelector(css selector)
+document.querySelectorAll(css selector)
+
+*/
+
+document.querySelectorAll('div')
+document.querySelectorAll('.buttons')
+document.getElementsByClassName('buttons')
+document.querySelector('.butons')
+document.querySelectorAll('div.buttons')
--- UD3/06_modify_dom/exercise01/main.js
+++ UD3/06_modify_dom/exercise01/main.js
@@ -1,5 +1,24 @@
 window.onload = function(e) {
     console.log('documento cargado')
 
-    // document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
+    document.querySelectorAll("h2")[1].innerText = "Cambiado";
+
+    document.getElementById("username");
+
+
+    document.querySelectorAll("article .first").forEach(element => {
+        element.style.color = "#3498db";
+      });
+
+      const listItems = document.querySelectorAll("li.item");
+
+      const buttons = document.querySelectorAll(".buttons button");
+
+      document.querySelector("p").style.backgroundColor = "#0000db"; 
+
+
+      buttons.forEach(button => {
+        button.style.color = "#1111db"; // Cambia "nuevoColorFrente" por el color que prefieras
+      });
+        //document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
 }
